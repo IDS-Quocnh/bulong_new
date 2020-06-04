9f2d215f12b3679cd71fd86941691f3b23c35aff
IDS_VN-Quocnh <quocnh@vn.ids.jp>
2020-06-04 20:53:14 +0700

    reinit source to public

---
+++ app/Model/Online.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class Online extends CModel
+{
+    protected $table = "online";
+}
---
+++ app/Model/Stat.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class Stat extends CModel
+{
+    protected $table = "statictis";
+}
